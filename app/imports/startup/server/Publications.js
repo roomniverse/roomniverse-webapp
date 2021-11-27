ab2aba854f96db114898d9a17d26bdf0ba87d9ea
Scott Vore <svore@hawaii.edu>
2021-11-26 18:56:27 -1000

    Update Publications.js

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -6,13 +6,6 @@ import { Users } from '../../api/user/User';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise publish nothing.
-Meteor.publish(Stuffs.userPublicationName, function () {
-  if (this.userId) {
-    const username = Meteor.users.findOne(this.userId).username;
-    return Stuffs.collection.find({ owner: username });
-  }
-  return this.ready();
-});
 
 Meteor.publish(Posts.userPublicationName, function () {
   if (this.userId) {
@@ -32,12 +25,6 @@ Meteor.publish(Users.userPublicationName, function () {
 
 // Admin-level publication.
 // If logged in and with admin role, then publish all documents from all users. Otherwise publish nothing.
-Meteor.publish(Stuffs.adminPublicationName, function () {
-  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-    return Stuffs.collection.find();
-  }
-  return this.ready();
-});
 
 /* Meteor.publish(Posts.adminPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {

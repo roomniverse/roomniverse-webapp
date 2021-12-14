ab309f189a3241dc575d7f9df049f481406eee68
tbui00 <buit@hawaii.edu>
2021-12-14 03:53:51 -1000

    combined tabs with the post and requests collection in user profile.

--- app/imports/ui/pages/UserProfile.jsx
+++ app/imports/ui/pages/UserProfile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Button, Container, Grid, Loader, Menu } from 'semantic-ui-react';
+import { Button, Container, Grid, Loader, Tab, Segment } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
@@ -8,6 +8,8 @@ import User from '../components/User';
 import { Users } from '../../api/user/User';
 import PostEvent from '../components/PostEvent';
 import { Posts } from '../../api/social/Posts';
+import Request from '../components/Request';
+import { Requests } from '../../api/social/Requests';
 
 class UserProfile extends React.Component {
 
@@ -16,6 +18,37 @@ class UserProfile extends React.Component {
   }
 
   renderPage() {
+    const panes = [
+      {
+        menuItem: 'Posts',
+        render: () => <Tab.Pane>
+          <div>
+            <Button id="addpost-button-profile" fluid as={Link} to='/add'>
+              Create a New Post
+            </Button>
+          </div>
+          <div style={{ marginTop: '20px' }}>
+            {this.props.posts.slice(0).reverse().map((post) => <PostEvent
+                key={post._id}
+                post={post}
+                user={this.props.doc}
+            />)}
+          </div>
+        </Tab.Pane>
+      },
+      {
+        menuItem: 'Request',
+        render: () => {
+          const requests = this.props.userRequest.find((request) => request.owner === this.props.currentUser);
+          return <Tab.Pane>
+              <Segment vertical>
+                <p>Location: {requests?.location}</p></Segment>
+              <Segment vertical>
+                <p>Description: {requests?.description}</p></Segment>
+          </Tab.Pane>
+        }
+      },
+    ]
     return (
       <div className="white-theme profile">
         <Container id="userprofile-page">
@@ -24,22 +57,8 @@ class UserProfile extends React.Component {
               <User user={this.props.doc}/>
             </Grid.Column>
             <Grid.Column width={12}>
-              <Menu pointing secondary borderless>
-                <Menu.Item link>Posts</Menu.Item>
-                <Menu.Item link>Roommate Inquires</Menu.Item>
-                <Menu.Item link>User Information</Menu.Item>
-              </Menu>
               <div>
-                <Button id="addpost-button-profile" fluid as={Link} to='/add'>
-                  Create a New Post
-                </Button>
-              </div>
-              <div style={{ marginTop: '20px' }}>
-                {this.props.posts.slice(0).reverse().map((post) => <PostEvent
-                  key={post._id}
-                  post={post}
-                  user={this.props.doc}
-                />)}
+                <Tab panes={panes}/>
               </div>
             </Grid.Column>
           </Grid>
@@ -53,19 +72,25 @@ UserProfile.propTypes = {
   posts: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
   doc: PropTypes.object,
+  userRequest: PropTypes.array.isRequired,
+  currentUser: PropTypes.string.isRequired,
 };
 
 export default withTracker(({ match }) => {
   const documentId = match.params._id;
-  const subscription = Meteor.subscribe(Posts.userPublicationName) && Meteor.subscribe(Users.userPublicationName);
+  const subscription = Meteor.subscribe(Posts.userPublicationName) && Meteor.subscribe(Users.userPublicationName) && Meteor.subscribe(Requests.userPublicationName);
   const ready = subscription.ready();
   const users = Users.collection.find({}).fetch();
   const doc = users.find((user) => user._id === documentId);
   const postCollection = Posts.collection.find({ }).fetch();
   const posts = postCollection.filter((post) => post.owner === doc.owner);
+  const userRequest = Requests.collection.find({}).fetch();
+  const currentUser = Meteor.user() ? Meteor.user().username : '';
   return {
     posts,
     ready,
     doc,
+    userRequest,
+    currentUser,
   };
 })(UserProfile);

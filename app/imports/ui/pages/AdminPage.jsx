c494845133c9f7097a6a0b5c70804f1dc2c2d8d3
Ju-Bin Choi <jubin0911@gmail.com>
2021-12-15 19:20:17 -1000

    Update AdminPage.jsx

--- app/imports/ui/pages/AdminPage.jsx
+++ app/imports/ui/pages/AdminPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Header, Loader, Grid, Item, Container } from 'semantic-ui-react';
+import { Header, Loader, Grid, Item } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Users } from '../../api/user/User';
@@ -18,54 +18,55 @@ class AdminPage extends React.Component {
 
   renderPage() {
     return (
-        <div id="admin-three-lists">
-            <Header as='h1'
-                    textAlign='center'
-                    style={{ padding: "20px 10px 10px 10px" }}>
-              Admin Page
-            </Header>
-            <Grid colummns={3} celled>
-              <Grid.Row>
-                <Grid.Column width={3} textAlign='center'>
-                  <Header as='h3'>Users</Header></Grid.Column>
-                <Grid.Column width={6} textAlign='center'>
-                  <Header as='h3'>Posts</Header></Grid.Column>
-                <Grid.Column width={7} textAlign='center'>
-                  <Header as='h3'>Requests</Header></Grid.Column>
-              </Grid.Row>
-              <Grid.Row stretched>
-                <Grid.Column width={3}>
-                  <div id="users-list-column">
-                  {this.props.users.map((user, index) => (
-                      <div className="all-positioning">
-                      <User key={index} user={user}/>
-                      </div>
+      <div id="admin-three-lists">
+        <Header as='h1'
+          textAlign='center'
+          style={{ padding: '20px 10px 10px 10px' }}>
+          Admin Page
+        </Header>
+        <Grid colummns={3} celled>
+          <Grid.Row>
+            <Grid.Column width={3} textAlign='center'>
+              <Header as='h3'>Users</Header></Grid.Column>
+            <Grid.Column width={6} textAlign='center'>
+              <Header as='h3'>Posts</Header></Grid.Column>
+            <Grid.Column width={7} textAlign='center'>
+              <Header as='h3'>Requests</Header></Grid.Column>
+          </Grid.Row>
+          <Grid.Row stretched>
+            <Grid.Column width={3}>
+              <div id="users-list-column">
+                {this.props.users.map((user, index) => (
+                  // eslint-disable-next-line react/jsx-key
+                  <div className="all-positioning">
+                    <User key={index} user={user}/>
+                  </div>
+                ))}
+              </div>
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <div id="posts-list-column">
+                {<div className="all-positioning">
+                  {this.props.posts.slice(0).reverse().map((post) => <PostEvent
+                    key={post._id}
+                    post={post}
+                    user={this.props.users.find((user) => user.owner === post.owner)}
+                  />)}
+                </div>}
+              </div>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <div id="requests-list-column">
+                <Item.Group divided>
+                  {this.props.requests.map((request, index) => (
+                    <Request className="all-positioning" key={index} request={request}/>
                   ))}
-                  </div>
-                </Grid.Column>
-                <Grid.Column width={6}>
-                  <div id="posts-list-column">
-                  {<div className="all-positioning">
-                      {this.props.posts.slice(0).reverse().map((post) => <PostEvent
-                          key={post._id}
-                          post={post}
-                          user={this.props.users.find((user) => user.owner === post.owner)}
-                      />)}
-                    </div>}
-                  </div>
-                </Grid.Column>
-                <Grid.Column width={7}>
-                  <div id="requests-list-column">
-                    <Item.Group divided>
-                      {this.props.requests.map((request, index) => (
-                          <Request className="all-positioning" key={index} request={request}/>
-                      ))}
-                    </Item.Group>
-                  </div>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-        </div>
+                </Item.Group>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
     );
   }
 }
@@ -80,9 +81,9 @@ AdminPage.propTypes = {
 export default withTracker(() => {
   // Get access to users, posts, and requests documents.
   const subscription =
-      Meteor.subscribe(Users.userPublicationName)
-      && Meteor.subscribe(Posts.userPublicationName)
-      && Meteor.subscribe(Requests.userPublicationName);
+    Meteor.subscribe(Users.userPublicationName)
+    && Meteor.subscribe(Posts.userPublicationName)
+    && Meteor.subscribe(Requests.userPublicationName);
   // Determine if the subscription is ready
   const ready = subscription.ready();
   const sub2 = Meteor.subscribe(Users.userPublicationName);
